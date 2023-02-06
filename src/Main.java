cd2c2772b174cb2dab275a483c1b83ca43c25a17
Viacheslav Privalov <privalov2306@yandex.ru>
2023-02-06 12:45:54 +0300

    Done 5-7

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main {
         task2();
         task3();
         task4();
-//        task5();
-//        task6();
-//        task7();
+        task5();
+        task6();
+        task7();
 //        task8();
 //        task9();
 //        task10();
@@ -39,6 +39,32 @@ public class Main {
         System.out.println("Задача4");
         for (int i = 10; i > -11; i--) {
             System.out.println(i);
+        }
+    }
+
+    public static void task5() {
+        System.out.println("Задача5");
+        for (int i = 1904; i < 2096; i += 4) {
+            System.out.println(i + " год является високосным");
+        }
+    }
+
+    public static void task6() {
+        System.out.println("Задача6");
+        for (int i = 7; i < 99; i += 7) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void task7() {
+        System.out.println("Задача7");
+        for (int i = 1; i < 513; i *= 2) {
+            System.out.println(i);
+        }
+        System.out.println("Ещё один способ");
+        for (int j = 0; j < 10; j++) {
+            System.out.println(Math.pow(2, j));
         }
     }
 }
